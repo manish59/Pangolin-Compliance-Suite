2cd2b290c02ebfddb0321cbca270c3372bf6e977
manish59 <manishkumar.bobbili3@gmail.com>
2025-03-28 09:57:37 -0700

    fixed project listing

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-# app_name = "projects"
+app_name = "projects"
 
 urlpatterns = [
     # Project list view - main page with the table and search functionality
